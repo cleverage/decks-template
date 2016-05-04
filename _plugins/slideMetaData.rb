94cb757d4e9e738e5d48ee9fb7e425a3dc42e39b
Boris SCHAPIRA <borisschapira@gmail.com>
2016-05-04 21:50:39 +0200

    Getting the depth and the deck where a slide belong

--- _plugins/slideMetaData.rb
+++ _plugins/slideMetaData.rb
@@ -1,7 +1,29 @@
+require "nokogiri"
+
 class SlideMetaDataGenerator < Jekyll::Generator
   def generate(site)
-    site.collections["slides"].docs.each do |slide|
-      slide.data["depth"] = slide.relative_path.split('/').size
+    parser = Jekyll::Converters::Markdown.new(site.config)
+    #p site.collections
+    site.collections.each do |key, collection|
+      if key == "slides"
+        collection.docs.each do |slide|
+
+          # Get title from slide
+          doc = Nokogiri::HTML(parser.convert(slide.content))
+
+          xmlElement = doc.css('h1').first
+          xmlElement = doc.css('h2').first unless xmlElement
+          xmlElement = doc.css('h3').first unless xmlElement
+          xmlElement = doc.css('h4').first unless xmlElement
+          xmlElement = doc.css('h5').first unless xmlElement
+          xmlElement = doc.css('h6').first unless xmlElement
+          slide.data["title"] = xmlElement.content
+
+          # Get depth from slide path
+          arrayFilePath = slide.relative_path.split('/');
+          slide.data["depth"] = arrayFilePath.size - 2
+        end
+      end
     end
   end
 end
